cd693da9a3e20a081cd55e672e048b1e6bd8e998
MDAlviano <mdalviano.21122@gmail.com>
2024-09-22 08:59:38 +0700

    membuat Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaGithub } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+
+const Navbar = () => {
+  return <nav className="mb-20 flex items-center justify-between py-4">
+    <div className="flex flex-shrink-0 items-center">
+        <h3 className="text-white mx-6 text-xl font-bold">Alviano</h3>
+    </div>
+    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaGithub/>
+        <FaInstagram/>
+    </div>
+  </nav>
+}
+
+export default Navbar
