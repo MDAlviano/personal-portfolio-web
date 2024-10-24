585e022a75ec9ea43c18cdc1b996627d05bc08bd
MDAlviano <mdalviano.21122@gmail.com>
2024-10-24 20:07:46 +0700

    add motion in Projects.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './components/About'
 import Technologies from './components/Technologies'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import { Footer } from './components/Footer'
 
 const App = () => {
   return (
@@ -20,7 +19,6 @@ const App = () => {
         <Technologies/>
         <Projects/>
         <Contact/>
-        <Footer/>
       </div>
     </div>
   )
